081db9e15d44cb93cc9714039addb4b271fc4b87
Владимир Максимов <vy.maksimov@MacBook-AirVYMaksimov.local>
2023-08-09 01:25:40 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://fcba3fff-c11c-4898-82c1-3bbff1271820.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
